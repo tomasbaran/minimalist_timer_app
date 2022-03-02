8655a919ba1fe5d7958030965cc84112a6a6323d
Tomas Baran (MacBook Air) <tomas.baran@gmail.com>
2022-03-02 11:30:18 +0000

    pause() logic

--- lib/widgets/timer_container/timer_container_notifier.dart
+++ lib/widgets/timer_container/timer_container_notifier.dart
@@ -1,25 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:minimalist_timer_app/services/parse_service.dart';
+import 'package:minimalist_timer_app/services/service_locator.dart';
 import 'package:minimalist_timer_app/utils/constants.dart';
 import 'dart:async';
+
+import 'package:minimalist_timer_app/widgets/buttons_container/buttons_container_notifier.dart';
 
 class TimerContainerNotifier extends ValueNotifier<String> {
   TimerContainerNotifier() : super(ParseService().durationToTimerFormat(mkDefaultTimer));
 
   Timer? _timer;
   int _secondsLeft = ParseService().durationToSeconds(mkDefaultTimer);
+  bool _isPaused = false;
+
+  countDownUntilZero(Timer _localTimer) {
+    final _buttonsNotifier = getIt<ButtonsContainerNotifier>();
+    _secondsLeft--;
+    value = ParseService().secondsToTimerFormat(_secondsLeft);
+    if (_secondsLeft == 0) {
+      _localTimer.cancel();
+      _buttonsNotifier.value = ButtonsState.finished;
+    }
+  }
 
   play() {
+    if (_isPaused) unpause();
+
     _timer = Timer.periodic(oneSec, (_localTimer) {
-      _secondsLeft--;
-      value = ParseService().secondsToTimerFormat(_secondsLeft);
-      if (_secondsLeft == 0) {
+      if (!_isPaused) {
+        countDownUntilZero(_localTimer);
+      } else {
         _localTimer.cancel();
       }
     });
   }
 
-  pause() {}
+  unpause() => _isPaused = false;
+
+  pause() => _isPaused = true;
 
   reset() {}
 }
